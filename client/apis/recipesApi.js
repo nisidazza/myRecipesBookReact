488f5f254597eba5f90f47d9b983b118ba67297c
Nisida Azzalini <nisida.azzalini@gmail.com>
2019-12-30 12:44:05 +1300

    rename recipes.js in apis folder to recipesApi.js and uncomment apiGet function

--- client/apis/recipesApi.js
+++ client/apis/recipesApi.js
@@ -1,11 +1,17 @@
 import request from 'superagent'
 
+const recipesUrl = 'api/v1/recipes'
 const ingredientsUrl = '/api/v1/ingredients'
 
-// export function apiGetRecipes() {
-//     return request.get(recipesUrl)
-//     .then(res => res.body)
-// }
+
+export function apiGetRecipes() {
+    return request.get(recipesUrl)
+    .then(res => res.body)
+    .catch(() => {
+        throw Error ('you need to implement an API route for /api/v1/ingredients')
+    })
+}
+
 export function apiGetIngredients() {
     return request.get(ingredientsUrl)
         .then(res => res.body)
