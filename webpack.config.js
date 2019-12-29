7c0375c6400862014b66af18c4d775b51483943f
Nisida Azzalini <nisida.azzalini@gmail.com>
2019-12-29 15:35:39 +1300

    fix typo

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './client/index.js',
     output: {
         path: path.join(__dirname, 'public'),
-        filename: 'bundel.js'
+        filename: 'bundle.js'
     },
     mode:'development',
     module: {
