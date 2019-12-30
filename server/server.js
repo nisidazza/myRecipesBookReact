a111a0fcd1f17a13f2b889f6ef0b30b74839ada5
Nisida Azzalini <nisida.azzalini@gmail.com>
2019-12-30 15:38:33 +1300

    do webpack step on postinstall

--- server/server.js
+++ server/server.js
@@ -1,20 +1,14 @@
 const express = require('express')
 const routes = require('./routes')
-
 const server = express()
 
-
 // Middleware
-
 server.use(express.urlencoded({extended: true}))
 
 server.use(express.json())
 // Serve static files
 server.use(express.static('public'))
 
-// Routes
-
-//server.use('/api/v1/recipes', recipeRoutes)
 server.use('/api/v1', routes)
 
 
