cb1041922c3324bc2bb7ab28c1af19182bc5a1f7
Nisida Azzalini <nisida.azzalini@gmail.com>
2019-12-31 21:20:49 +1300

    fix bug (rename Search to IngredientsList)

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {HashRouter as Router, Route, Link} from 'react-router-dom'
-import Search from './IngredientsList'
+import IngredientsList from './IngredientsList'
 import RecipesList from './RecipesList'
 
 
