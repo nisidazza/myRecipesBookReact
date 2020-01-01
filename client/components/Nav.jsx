176b6507c3c6aaa0f28e93c1db87933c5918b93d
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-01-01 16:00:10 +1300

    implement Authenticated component and add it to Nav component

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-import {logOff} from 'authenticare/client'
+import { Link } from 'react-router-dom'
+import { logOff } from 'authenticare/client'
+import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
 
 export default function Nav() {
-    return(
+    return (
         <>
             <Link to='/'>Home</Link>
-            <Link to='/register'>Register</Link>
-            <Link to='signin'>Sign In</Link>
-            <Link to='#' onClick={logOff}>Log Off</Link>
+            <IfAuthenticated>
+                <Link to='#' onClick={logOff}>Log Off</Link>
+            </IfAuthenticated>
+            <IfNotAuthenticated>
+                <Link to='/register'>Register</Link>
+                <Link to='signin'>Sign In</Link>
+            </IfNotAuthenticated>
         </>
     )
 }
