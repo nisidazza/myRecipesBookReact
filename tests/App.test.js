c627d58cbe27d2df8bb17a588ffc4a30b8b9067f
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-01-02 21:39:57 +1300

    basic test infrastructure setup

---
+++ tests/App.test.js
@@ -0,0 +1,8 @@
+describe('My Test Suite', () => {
+    it('My Test Case', () => {
+        expect(true).toEqual(true)
+    }),
+    it('My Test Case2', () => {
+        expect(true).toEqual(true) 
+    })
+})
