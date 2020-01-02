20e8930a99a2b3376b5090c0bb098de20eef841d
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-01-02 22:50:46 +1300

    change password hash type to string

--- migrations/20191230180706_users.js
+++ migrations/20191230180706_users.js
@@ -3,7 +3,7 @@ exports.up = function (knex) {
     return knex.schema.createTable('users', function (table) {
         table.increments('id')
         table.string('username')
-        table.binary('hash')
+        table.string('hash')
     })
 };
 
