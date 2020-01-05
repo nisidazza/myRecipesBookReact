302dc80fab6923399c6202c7350613d231a02405
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-01-06 09:24:25 +1300

    create Recipe component to show recipe details

---
+++ client/components/Recipe.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class Recipe extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <h1>Hello</h1>
+        )
+    }
+
+}
+
+
+export default Recipe
