4056a786e2a2292c94adc68d192c7e875202ed01
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-01-22 20:46:12 +1300

    add new user

--- seeds/030_populate_users.js
+++ seeds/030_populate_users.js
@@ -8,7 +8,13 @@ exports.seed = knex =>
                     id: 1,
                     username: "Pippo",
                     hash: hashedPassword
-                }]).then(() => {
-                    knex.raw('SELECT setval("ingredients_id_seq", (SELECT MAX(id) from "test"))')
-                })
+                },
+                {
+                    id: 2,
+                    username: "Nisida",
+                    hash: hashedPassword
+                }
+            ]).then(() => {
+                knex.raw('SELECT setval("ingredients_id_seq", (SELECT MAX(id) from "test"))')
+            })
         )
