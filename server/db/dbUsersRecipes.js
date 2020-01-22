39615c1bff795067a8148c0054d65d3a218cf910
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-01-22 20:47:50 +1300

    add users recipes db function and routes

---
+++ server/db/dbUsersRecipes.js
@@ -0,0 +1,22 @@
+const connection = require('./connection')
+
+function getUserPrivateRecipes(userId, db = connection) {
+    return db('recipes')
+        .select()
+        .where('is_public', false)
+        .where('user_id', userId)
+        .orderBy('recipes.title')
+}
+
+function getUserRecipes(userId, db = connection) {
+    return db('recipes')
+        .select()
+        .where('user_id', userId)
+        .orderBy('recipes.title')
+}
+
+
+module.exports = {
+    getUserPrivateRecipes,
+    getUserRecipes
+}
---
+++ server/routes/usersRoutes.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router()
+
+const dbUsersRecipes = require('../db/dbUsersRecipes')
+
+
+//GET /api/v1/recipes/user/:id
+
+router.get('/:id', (req, res) => {
+    const userId = req.params.id
+    dbUsersRecipes.getUserRecipes(userId)
+        .then(userRecipes => {
+            res.json(userRecipes)
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Something is broken' })
+        })
+})
+
+
+//GET /api/v1/recipes/user/:id/private
+
+router.get('/:id/private', (req, res) => {
+    const userId = req.params.id
+    dbUsersRecipes.getUserPrivateRecipes(userId)
+        .then(privateRecipes => {
+            res.json(privateRecipes)
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Something is broken' })
+        })
+})
+
+
+
+
+module.exports = router
