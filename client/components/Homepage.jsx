b6727492ab79baeb098c30b02206dd73e081dd6c
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-02-04 17:50:56 +1300

    add vertical sidebar to homepage and style

--- client/components/Homepage.jsx
+++ client/components/Homepage.jsx
@@ -1,18 +1,50 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 class Homepage extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props);
+  }
 
-
-    render() {
-        return (
-            <div id='Homepage-jsx-component'>
-                <h1>Welcome to my Homepage</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="Homepage-jsx-component">
+        <nav className="navbar">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/recipes/list">
+                Recipes List
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Add Recipe
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Search
+              </a>
+            </li>
+          </ul>
+        </nav>
+        {/* <div className="container mt-5 mb-5">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="input-group">
+                                <input className="form-control border-secondary py-2" type="text" placeholder="search" />
+                                <div className="input-group-append">
+                                    <button className="btn btn-outline-secondary" type="button">
+                                        <i className="fa fa-search"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> */}
+      </div>
+    );
+  }
 }
 
-export default Homepage
+export default Homepage;
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,76 +1,88 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { logOff, getDecodedToken } from 'authenticare/client'
-import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
+import React from "react";
+import { Link } from "react-router-dom";
+import { logOff, getDecodedToken } from "authenticare/client";
+import { IfAuthenticated, IfNotAuthenticated } from "./Authenticated";
 
 class Nav extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
+  }
+
+  getUserName = () => {
+    let myToken = getDecodedToken();
+    if (myToken) {
+      return myToken.username;
+    } else {
+      return "";
     }
+  };
 
-    getUserName = () => {
-        let myToken = getDecodedToken()
-        if (myToken) {
-            return myToken.username
-        } else {
-            return ''
-        }
-    }
+  handleLogOff = () => {
+    logOff();
+    this.props.history.push("/");
+  };
 
-    handleLogOff = () => {
-        logOff()
-        this.props.history.push('/')
-    }
+  render() {
+    return (
+      <div id="Nav-jsx-component">
+        <div
+          className="jumbotron-fluid text-center"
+          style={{ marginBottom: "0" }}
+        >
+          <h1 className="display-4 " id="title">
+            My Recipe Book
+          </h1>
+          <h4 id="statement">Keep track of your favourite recipes!</h4>
+        </div>
+        <nav className="navbar fixed navbar-expand-sm">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/homepage">
+                  Homepage
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-    render() {
-        return (
-            <div id="Nav-jsx-component">
-                <div className='jumbotron-fluid text-center' style={{ marginBottom: '0' }}>
-                    <h1 className='display-4 ' id='title'>My Recipe Book</h1>
-                    <h4 id='statement' >Keep track of your favourite recipes!</h4>
-                </div>
-                <nav className='navbar fixed navbar-expand-sm'>
-                    <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-                        <ul className='navbar-nav'>
-                            <li className='nav-item'>
-                                <Link className='nav-link' to='/homepage'>Homepage</Link>
-                            </li>
-                        </ul>
-
-                        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-                            <ul className='navbar-nav'>
-                                <li className='nav-item'>
-                                    <Link className='nav-link' to='/recipes/list'>Recipes List</Link>
-                                </li>
-                                {/* <li className='nav-item'>
-                                        <Link className='nav-link' to='#'>Add New Recipe</Link>
-                                    </li> */}
-                            </ul>
-                        </div>
-                        <IfAuthenticated>
-                            <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-                                <ul className='navbar-nav ml-auto'>
-                                    <li className='nav-item p-2 mt-2'><h5 className='nav-item'>Welcome, {this.getUserName()}!</h5></li>
-                                    <li className='nav-item p-0 mt-2'>
-                                        <Link to='#' className='nav-link' onClick={this.handleLogOff}>Log Off</Link>
-                                    </li>
-                                </ul>
-                            </div>
-                        </IfAuthenticated>
-
-                        <ul className='navbar-nav ml-auto'>
-                            <IfNotAuthenticated>
-                                <div className='d-flex p-2'>
-                                    <li className='nav-item w-100 pl-2'><Link to='/register' className='nav-link'>Register</Link></li>
-                                    <li className='nav-item w-100 pl-2'><Link to='/signin' className='nav-link'>Sign In</Link></li>
-                                </div>
-                            </IfNotAuthenticated>
-                        </ul>
-                    </div>
-                </nav>
+          <IfAuthenticated>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item p-2 mt-2">
+                  <h5 className="nav-item">Welcome, {this.getUserName()}!</h5>
+                </li>
+                <li className="nav-item p-0 mt-2">
+                  <Link to="#" className="nav-link" onClick={this.handleLogOff}>
+                    Log Off
+                  </Link>
+                </li>
+              </ul>
             </div>
-        )
-    }
+          </IfAuthenticated>
+
+          <ul className="navbar-nav ml-auto">
+            <IfNotAuthenticated>
+              <div className="d-flex p-2">
+                <li className="nav-item w-100 pl-2">
+                  <Link to="/register" className="nav-link">
+                    Register
+                  </Link>
+                </li>
+                <li className="nav-item w-100 pl-2">
+                  <Link to="/signin" className="nav-link">
+                    Sign In
+                  </Link>
+                </li>
+              </div>
+            </IfNotAuthenticated>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
 }
 
-export default Nav
+export default Nav;
