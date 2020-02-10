e6862bd3a72110dfa349544b13d203739cf22c5b
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-02-10 16:18:53 +1300

    working on adding new recipe

--- server/db/dbRecipes.js
+++ server/db/dbRecipes.js
@@ -1,71 +1,73 @@
-const connection = require('./connection')
+const connection = require("./connection");
 
-function addRecipe(title, category, link, notes, db = connection) {
-    return db('recipes')
-        .insert({
-            title,
-            category,
-            link,
-            notes
-        }, 'id')
+function addRecipe(newRecipe, db = connection) {
+  return db("recipes")
+    .insert(
+      newRecipe, ['*']
+  );
 }
 
 function deleteRecipe(id, db = connection) {
-    return db('recipes')
-        .where('id', id).first()
-        .delete()
+  return db("recipes")
+    .where("id", id)
+    .first()
+    .delete();
 }
 
 function editRecipe(id, newRecipe, db = connection) {
-    return db('recipes')
-        .where('id', id)
-        .select()
-        .then(oldRecipe => {
-            if (oldRecipe.length == 0) return { hasBeenUpdated: false }
-            return db('recipes')
-                .where('id', id)
-                .update({
-                    title: newRecipe.title ? newRecipe.title : oldRecipe[0].title,
-                    category: newRecipe.category ? newRecipe.category : oldRecipe[0].category,
-                    link: newRecipe.link ? newRecipe.link : oldRecipe[0].link,
-                    notes: newRecipe.notes ? newRecipe.notes : oldRecipe[0].notes
-                }, ['*'])
-                .then(updatedRecipes => {
-                    let hasBeenUpdated = updatedRecipes && updatedRecipes.length > 0
-                    let newRecipe = hasBeenUpdated ? updatedRecipes[0] : null
-                    return {
-                        hasBeenUpdated,
-                        newRecipe
-                    }
-                })
-        })
+  return db("recipes")
+    .where("id", id)
+    .select()
+    .then(oldRecipe => {
+      if (oldRecipe.length == 0) return { hasBeenUpdated: false };
+      return db("recipes")
+        .where("id", id)
+        .update(
+          {
+            title: newRecipe.title ? newRecipe.title : oldRecipe[0].title,
+            category: newRecipe.category
+              ? newRecipe.category
+              : oldRecipe[0].category,
+            link: newRecipe.link ? newRecipe.link : oldRecipe[0].link,
+            notes: newRecipe.notes ? newRecipe.notes : oldRecipe[0].notes
+          },
+          ["*"]
+        )
+        .then(updatedRecipes => {
+          let hasBeenUpdated = updatedRecipes && updatedRecipes.length > 0;
+          let newRecipe = hasBeenUpdated ? updatedRecipes[0] : null;
+          return {
+            hasBeenUpdated,
+            newRecipe
+          };
+        });
+    });
 }
 
 function getPublicRecipes(db = connection) {
-    return db('recipes')
-        .select()
-        .where('is_public', true)
-        .orderBy('recipes.title')
+  return db("recipes")
+    .select()
+    .where("is_public", true)
+    .orderBy("recipes.title");
 }
 
 function getListRecipes(db = connection) {
-    return db('recipes')
-        .select()
-        .orderBy('recipes.title')
+  return db("recipes")
+    .select()
+    .orderBy("recipes.title");
 }
 
 function getRecipe(id, db = connection) {
-    return db('recipes')
-        .where('id', id).first()
+  return db("recipes")
+    .where("id", id)
+    .first();
 }
-
-
 
 module.exports = {
-    addRecipe,
-    deleteRecipe,
-    editRecipe,
-    getPublicRecipes,
-    getListRecipes,
-    getRecipe,
-}
+  addRecipe,
+  deleteRecipe,
+  editRecipe,
+  getPublicRecipes,
+  getListRecipes,
+  getRecipe
+};
--- server/routes/recipesRoutes.js
+++ server/routes/recipesRoutes.js
@@ -1,135 +1,158 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const dbRecipes = require('../db/dbRecipes')
-const dbUserRecipes = require('../db/dbUsersRecipes')
-const dbRecipesIngredients = require('../db/dbRecipesIngredients')
+const dbRecipes = require("../db/dbRecipes");
+const dbUserRecipes = require("../db/dbUsersRecipes");
+const dbRecipesIngredients = require("../db/dbRecipesIngredients");
 
-const { getTokenDecoder } = require('authenticare/server')
+const { getTokenDecoder } = require("authenticare/server");
 
-// GET /apiv1/recipes/public
-router.get('/public', (req, res) => {
-    dbRecipes.getPublicRecipes()
-        .then(publicRecipes => {
-            res.json(publicRecipes)
-        })
-        .catch(err => {
-            res.status(500).json({ message: 'Something is broken' })
-            console.log(err)
-        })
-})
-
+// GET /api/v1/recipes/public
+router.get("/public", (req, res) => {
+  dbRecipes
+    .getPublicRecipes()
+    .then(publicRecipes => {
+      res.json(publicRecipes);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Something is broken" });
+      console.log(err);
+    });
+});
 
 //GET /api/v1/recipes --- all public and user private
-router.get('/', getTokenDecoder(false), (req, res) => {
-    dbRecipes.getPublicRecipes()
-        .then(publicRecipes => {
-            if (req.user) {
-                dbUserRecipes.getUserPrivateRecipes(req.user.id)
-                    .then(privateRecipes => {
-                        const recipes = publicRecipes.concat(privateRecipes)
-                        res.json(recipes)
-                    })
-            } else {
-                res.json(publicRecipes)
-            }
-        })
-        .catch(err => {
-            res.status(500).json({ message: 'Something is broken' })
-            console.log(err)
-        })
-})
+router.get("/", getTokenDecoder(false), (req, res) => {
+  dbRecipes
+    .getPublicRecipes()
+    .then(publicRecipes => {
+      if (req.user) {
+        dbUserRecipes
+          .getUserPrivateRecipes(req.user.id)
+          .then(privateRecipes => {
+            const recipes = publicRecipes.concat(privateRecipes);
+            res.json(recipes);
+          });
+      } else {
+        res.json(publicRecipes);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Something is broken" });
+      console.log(err);
+    });
+});
 
-router.get('/private', getTokenDecoder(), (req, res) => {
-    const loggedUserId = req.user.id
-    dbUserRecipes.getUserPrivateRecipes(loggedUserId)
-        .then(privateRecipes => {
-            res.json(privateRecipes)
-        })
-        .catch(err => {
-            res.status(500).json({ message: 'Something is broken' })
-            console.log(err)
-        })
-})
+router.get("/private", getTokenDecoder(), (req, res) => {
+  const loggedUserId = req.user.id;
+  dbUserRecipes
+    .getUserPrivateRecipes(loggedUserId)
+    .then(privateRecipes => {
+      res.json(privateRecipes);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Something is broken" });
+      console.log(err);
+    });
+});
 
 // GET /api/v1/recipes/:id
-router.get('/:id', getTokenDecoder(false), (req, res) => {
-    const { id } = req.params
-    dbRecipes.getRecipe(id)
-        .then(recipeDetail => {
-            if (recipeDetail) {
-                const userIsAuthenticatedAndMatches = (req.user && (req.user.id == id))
-                if (recipeDetail.is_public || userIsAuthenticatedAndMatches) {
-                    dbRecipesIngredients.getIngredients(id)
-                        .then(ingredients => {
-                            res.json({ ...recipeDetail, ingredients })
-                        })
-                }
-            } else {
-                res.sendStatus(404)
-            }
-        })
-        .catch(err => {
-            res.status(500).json({ message: 'Something is broken' })
-            console.log(err)
-        })
-})
+router.get("/:id", getTokenDecoder(false), (req, res) => {
+  const { id } = req.params;
+  dbRecipes
+    .getRecipe(id)
+    .then(recipeDetail => {
+      if (recipeDetail) {
+        const userIsAuthenticatedAndMatches = req.user && req.user.id == id;
+        if (recipeDetail.is_public || userIsAuthenticatedAndMatches) {
+          dbRecipesIngredients.getIngredients(id).then(ingredients => {
+            res.json({ ...recipeDetail, ingredients });
+          });
+        }
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Something is broken" });
+      console.log(err);
+    });
+});
 
 // DELETE /api/v1/recipes/:id
-router.delete('/:id', getTokenDecoder(), (req, res) => {
-    const { id } = req.params
-    const loggedUserId = req.user.id
-    dbRecipes.getRecipe(id)
-        .then(recipe => {
-            if (recipe) {
-                if (loggedUserId === recipe.user_id) {
-                    dbRecipes.deleteRecipe(id)
-                        .then(hasBeenDeleted => {
-                            if (hasBeenDeleted) {
-                                res.sendStatus(204)
-                            } else {
-                                res.sendStatus(404)
-                            }
-                        })
-                } else {
-                    console.log(`user ${req.user.username} cannot delete recipe ${recipe.title} `)
-                    res.sendStatus(403)
-                }
+router.delete("/:id", getTokenDecoder(), (req, res) => {
+  const { id } = req.params;
+  const loggedUserId = req.user.id;
+  dbRecipes
+    .getRecipe(id)
+    .then(recipe => {
+      if (recipe) {
+        if (loggedUserId === recipe.user_id) {
+          dbRecipes.deleteRecipe(id).then(hasBeenDeleted => {
+            if (hasBeenDeleted) {
+              res.sendStatus(204);
             } else {
-                res.sendStatus(404)
+              res.sendStatus(404);
             }
-        })
-        .catch(err => {
-            res.status(500).json({ message: 'Something is broken' })
-            console.log(err)
-        })
-})
+          });
+        } else {
+          console.log(
+            `user ${req.user.username} cannot delete recipe ${recipe.title} `
+          );
+          res.sendStatus(403);
+        }
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Something is broken" });
+      console.log(err);
+    });
+});
 
+router.patch("/:id", getTokenDecoder(), (req, res) => {
+  const { id } = req.params;
+  const recipe = req.body;
+  const loggedUser = req.user.id;
+  if (loggedUser === recipe.user_id) {
+    //console.log('username:', req.user.username)
+    dbRecipes
+      .editRecipe(id, recipe)
+      .then(({ hasBeenUpdated, newRecipe }) => {
+        if (hasBeenUpdated) {
+          res.status(200).json(newRecipe);
+        } else {
+          res.sendStatus(404);
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: "Something is broken" });
+        console.log(err);
+      });
+  } else {
+    console.log(
+      `user ${req.user.username} cannot edit recipe ${recipe.title} `
+    );
+    res.sendStatus(403);
+  }
+});
 
-router.patch('/:id', getTokenDecoder(), (req, res) => {
-    const { id } = req.params
-    const recipe = req.body
-    const loggedUser = req.user.id
-    if (loggedUser === recipe.user_id) {
-        //console.log('username:', req.user.username)
-        dbRecipes.editRecipe(id, recipe)
-            .then(({ hasBeenUpdated, newRecipe }) => {
-                if (hasBeenUpdated) {
-                    res.status(200).json(newRecipe)
-                } else {
-                    res.sendStatus(404)
-                }
-            })
-            .catch(err => {
-                res.status(500).json({ message: 'Something is broken' })
-                console.log(err)
-            })
-    } else {
-        console.log(`user ${req.user.username} cannot edit recipe ${recipe.title} `)
-        res.sendStatus(403)
-    }
-})
+router.post("/", getTokenDecoder(), (req, res) => {
+  let newRecipe = req.body;
+  newRecipe.user_id = req.user.id,
 
+  dbRecipes
+    .addRecipe(newRecipe)
+    .then(res.sendStatus(200))
+    // dbRecipesIngredients
+    //   .addIngredientToRecipe(newRecipeId, ingredient_id, quantity)
+    //   .then(ingredients => {
+    //     res.json({ ...newRecipe, ingredients });
+    //   });
+    .catch(err => {
+      res.status(500).json({ message: "Something is broken" });
+      console.log(err);
+    });
+});
 
-
-module.exports = router
+module.exports = router;
