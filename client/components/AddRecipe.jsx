70eadea215dff859bad36887f6698468b52f6a97
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-02-12 09:14:58 +1300

    create AddRecipe component

---
+++ client/components/AddRecipe.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class AddRecipe extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+
+    render() {
+        return(
+            <h1>Add New Recipe!</h1>
+        )
+    }
+}
+
+
+export default AddRecipe
