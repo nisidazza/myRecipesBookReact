1750baf04f8c6cae0a28b52a89e2bc784bf2d58e
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-02-28 11:09:34 +1300

    create api client request to get all the recipes that include at least one of the ingredients ids pass to the query  string

--- client/apis/recipesSearchApi.js
+++ client/apis/recipesSearchApi.js
@@ -3,10 +3,23 @@ const recipesSearchUrl = "api/v1/recipes-search";
 
 export function apiGetRecipesMatchingAllIngredients(ingredient_ids) {
   return httpClient
-    .get(`${recipesSearchUrl}?ingredient_ids=${ingredient_ids}`)
+    .get(`${recipesSearchUrl}/match-all?ingredient_ids=${ingredient_ids}`)
     .catch(() => {
       throw Error(
-        "you need to implement an API route for /api/v1/recipes-search"
+        "you need to implement an API route for /api/v1/recipes-search/match-all"
+      );
+    })
+    .then(res => {
+      return res.body;
+    });
+}
+
+export function apiGetRecipesByIngredients(ingredient_ids) {
+  return httpClient
+    .get(`${recipesSearchUrl}/match?ingredient_ids=${ingredient_ids}`)
+    .catch(() => {
+      throw Error(
+        "you need to implement an API route for /api/v1/recipes-search/match-all"
       );
     })
     .then(res => {
