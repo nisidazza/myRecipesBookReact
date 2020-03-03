b64bcbb97be7c601d21496ace378892fe53a398b
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-03-03 20:16:22 +1300

    create cloudinary config file

---
+++ server/cloudinaryConfig.js
@@ -0,0 +1,10 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "hqwayz2au",
+  api_key: "962548597672457",
+  api_secret: "TrCeEPYL6HSvL8KQrp5SYBWtnhc"
+});
+
+
+module.exports = cloudinary
