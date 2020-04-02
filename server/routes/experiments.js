bac8202164abc19356e47fa4791f9eec41fbf7c9
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-04-02 14:26:31 +1300

    test post route to send email

--- server/routes/experiments.js
+++ server/routes/experiments.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 
+require('dotenv').config()
+mailgun = require('mailgun-js')({ apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN})
+
+
+
 const cloudinary = require('../cloudinaryConfig')
 
 router.post("/upload", upload.single("myFile"), (req, res) => {
@@ -12,5 +17,22 @@ router.post("/upload", upload.single("myFile"), (req, res) => {
       res.json({message : image.url})
   })
 });
+
+
+router.post('/email', (req,res) => {
+
+  const data = {
+    from: 'Excited User <me@samples.mailgun.org>',
+    to: 'nisida.azzalini@gmail.com',
+    subject: 'Hello',
+    text: 'Testing some Mailgun awesomeness!'
+  };
+  mailgun.messages().send(data, function (error, mailgunResponse) {
+    console.log(mailgunResponse);
+  });
+})
+
+
+
 
 module.exports = router;
