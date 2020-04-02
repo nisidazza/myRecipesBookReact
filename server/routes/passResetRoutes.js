08a09e10b2c3303ef8237ab663a26ee7cdfde365
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-04-02 18:14:44 +1300

    add callbackURL to reset-password post route

--- server/routes/passResetRoutes.js
+++ server/routes/passResetRoutes.js
@@ -3,24 +3,21 @@ const router = express.Router();
 
 const passwordReset = require("../passResetConfig");
 
-
 //Route for requesting a new reset token
-
 router.post(
   "/password/reset",
   passwordReset.requestResetToken({
     loginParam: "login",
-    callbackURL: "/{token}",
+    callbackURL: "/resetPassword/?token={token}",
     next: false
   }),
   function(req, res) {
     // ...something
-    res.sendStatus(404)
+    res.sendStatus(404);
   }
 );
 
 //Route for actually reseting passwords
-
 router.put(
   "/password/reset",
   passwordReset.resetPassword({
@@ -33,5 +30,7 @@ router.put(
     // ...something
   }
 );
+
+
 
 module.exports = router;
