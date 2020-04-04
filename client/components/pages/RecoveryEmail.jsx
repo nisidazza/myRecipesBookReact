28fc7da25e98162a76d2ca6f9c5f43d1f41ea388
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-04-04 16:47:14 +1300

    add outcome state - a specific message is displayed based on successful/unsuccessful submission

--- client/components/pages/RecoveryEmail.jsx
+++ client/components/pages/RecoveryEmail.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-
+import { apiRequestResetToken } from "../../apis/resetPasswordApi";
 
 class RecoveryEmail extends React.Component {
+  possibleOutcomes = {
+    undefined: undefined,
+    success: "success",
+    error: "error"
+  };
+
   constructor(props) {
     super(props);
 
-    this.recoveryEmail = {
-      username: ""
-    }
-
+    this.state = {
+      username: "",
+      outcome: this.possibleOutcomes.undefined
+    };
   }
 
   componentDidMount() {
@@ -19,25 +25,35 @@ class RecoveryEmail extends React.Component {
   }
 
   handleChange = e => {
-    this.recoveryEmail[e.target.id] = e.target.value;
-    console.log(this.recoveryEmail[e.target.id])
+    this.state[e.target.id] = e.target.value;
+    console.log(this.state[e.target.id]);
   };
 
-  handleRecoveryEmailSubmit = (e) => {
+  handleRecoveryEmailSubmit = e => {
     e.preventDefault();
-
-  }
+    apiRequestResetToken(this.state.username).then(outcome => {
+      console.log(outcome);
+      this.setState({
+        outcome: outcome.success
+          ? this.possibleOutcomes.success
+          : this.possibleOutcomes.error
+      });
+    });
+  };
 
   render() {
     return (
       <div id="RecoveryEmail-jsx-component">
-        <form className="mx-auto" style={{ maxWidth: "500px", marginTop: "100px" }}
-        onSubmit={this.handleRecoveryEmailSubmit}>
+        <form
+          className="mx-auto"
+          style={{ maxWidth: "500px", marginTop: "100px" }}
+          onSubmit={this.handleRecoveryEmailSubmit}
+        >
           <div className="text-center">
             <h4>Forgot Password?</h4>
             <p>
-              No worries! Just enter your email address and we'll send you a
-              reset password link.
+              No worries! Just enter the email address you provided and we'll
+              send you a reset password link.
             </p>
           </div>
           <div className="border mt-4" id="border-shadow">
@@ -54,11 +70,27 @@ class RecoveryEmail extends React.Component {
               </div>
             </div>
             <div className="input-container mx-auto mt-3 mb-3 col-xs-3">
-              <input
-                value="Send Recovery Email"
-                type="submit"
-                className="btn btn-outline-success mx-auto"
-              />
+              {this.state.outcome !== this.possibleOutcomes.success ? (
+                <input
+                  value="Send Recovery Email"
+                  type="submit"
+                  className="btn btn-outline-success mx-auto"
+                />
+              ) : (
+                ""
+              )}
+            </div>
+            <div>
+              {this.state.outcome == this.possibleOutcomes.success ? (
+                <p>
+                  “An email has been sent to {this.state.username} with further
+                  instructions.”
+                </p>
+              ) : this.state.outcome == this.possibleOutcomes.error ? (
+                <p>"Something went wrong"</p>
+              ) : (
+                ""
+              )}
             </div>
           </div>
         </form>
