f258b7596edf7a40f533db8874d2573f5b314e5f
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-05-12 13:58:52 +1200

    add "is_reviewed" to ingredients seeds

--- seeds/020_populate_ingredients.js
+++ seeds/020_populate_ingredients.js
@@ -3,470 +3,587 @@ exports.seed = async knex => {
         {
             id: 1,
             name: 'flour',
+            is_reviewed: true
         },
         {
             id: 2,
             name: 'sugar',
+            is_reviewed: true
         },
         {
             id: 3,
             name: 'baking powder',
+            is_reviewed: true
         },
         {
             id: 4,
             name: 'salt',
+            is_reviewed: true
         },
         {
             id: 5,
             name: 'eggs',
+            is_reviewed: true
         },
         {
             id: 6,
             name: 'milk',
+            is_reviewed: true
         },
         {
             id: 7,
             name: 'extra-vergin olive oil',
+            is_reviewed: true
         },
         {
             id: 8,
             name: 'chicken or vegetable broth',
+            is_reviewed: true
         },
         {
             id: 9,
             name: 'onion',
+            is_reviewed: true
         },
         {
             id: 10,
             name: 'butter',
+            is_reviewed: true
         },
         {
             id: 11,
             name: 'clove garlic',
+            is_reviewed: true
         },
         {
             id: 12,
             name: 'mushroom',
+            is_reviewed: true
         },
         {
             id: 13,
             name: 'parsley',
+            is_reviewed: true
         },
         {
             id: 14,
             name: 'rice',
+            is_reviewed: true
         },
         {
             id: 15,
             name: 'white wine',
+            is_reviewed: true
         },
         {
             id: 16,
             name: 'parmesan cheese',
+            is_reviewed: true
         },
         {
             id: 17,
             name: 'black pepper',
+            is_reviewed: true
         },
         {
             id: 18,
             name: 'yeast',
+            is_reviewed: true
         },
         {
             id: 19,
             name: 'tomato sauce',
+            is_reviewed: true
         },
         {
             id: 20,
             name: 'ham',
+            is_reviewed: true
         },
         {
             id: 21,
             name: 'mozzarella',
+            is_reviewed: true
         },
         {
             id: 22,
             name: 'king prawns',
+            is_reviewed: true
         },
         {
             id: 23,
             name: 'basil',
+            is_reviewed: true
         },
         {
             id: 24,
             name: 'water',
+            is_reviewed: true
         },
         {
             id: 25,
             name: 'cabbage',
+            is_reviewed: true
         },
         {
             id: 26,
             name: 'pork mince',
+            is_reviewed: true
         },
         {
             id: 27,
             name: 'spring onions',
+            is_reviewed: true
         },
         {
             id: 28,
             name: 'soy sauce',
+            is_reviewed: true
         },
         {
             id: 29,
             name: 'minced ginger',
+            is_reviewed: true
         },
         {
             id: 30,
             name: 'sesame oil',
+            is_reviewed: true
         },
         {
             id: 31,
             name: 'dumpling wrappers',
+            is_reviewed: true
         },
         {
             id: 32,
             name: 'cocoa powder',
+            is_reviewed: true
         },
         {
             id: 33,
             name: 'vanilla essence',
+            is_reviewed: true
         },
         {
             id: 34,
             name: 'oregano',
+            is_reviewed: true
         },
         {
             id: 35,
-            name: 'pie crust'
+            name: 'pie crust',
+            is_reviewed: true
         },
         {
             id: 36,
-            name: 'slices bacon'
+            name: 'slices bacon',
+            is_reviewed: true
         },
         {
             id: 37,
-            name: 'Swiss or Cheddar cheese'
+            name: 'Swiss or Cheddar cheese',
+            is_reviewed: true
         },
         {
             id: 38,
-            name: ' baguette'
+            name: ' baguette',
+            is_reviewed: true
         },
         {
             id: 39,
-            name: 'lemon juice'
+            name: 'lemon juice',
+            is_reviewed: true
         },
         {
             id: 40,
-            name: 'romaine lettuce'
+            name: 'romaine lettuce',
+            is_reviewed: true
         },
         {
             id: 41,
-            name: 'croutons'
+            name: 'croutons',
+            is_reviewed: true
         },
         {
             id: 42,
-            name: 'medium onions'
+            name: 'medium onions',
+            is_reviewed: true
         },
         {
             id: 43,
-            name: 'celery stalks'
+            name: 'celery stalks',
+            is_reviewed: true
         },
         {
             id: 44,
-            name: 'carrots'
+            name: 'carrots',
+            is_reviewed: true
         },
         {
             id: 45,
-            name: 'beef mince'
+            name: 'beef mince',
+            is_reviewed: true
         },
         {
             id: 46,
-            name: 'veal mince'
+            name: 'veal mince',
+            is_reviewed: true
         },
         {
             id: 47,
-            name: 'red wine'
+            name: 'red wine',
+            is_reviewed: true
         },
         {
             id: 48,
-            name: 'beef or chicken stock'
+            name: 'beef or chicken stock',
+            is_reviewed: true
         },
         {
             id: 49,
-            name: 'tagliatelle or fettuccine'
+            name: 'tagliatelle or fettuccine',
+            is_reviewed: true
         },
         {
             id: 50,
-            name: 'potatoes'
+            name: 'potatoes',
+            is_reviewed: true
         },
         {
             id: 51,
-            name: 'Cheddar cheese'
+            name: 'Cheddar cheese',
+            is_reviewed: true
         },
         {
             id: 52,
-            name: 'rosemary'
+            name: 'rosemary',
+            is_reviewed: true
         },
         {
             id: 53,
-            name: 'thyme'
+            name: 'thyme',
+            is_reviewed: true
         },
         {
             id: 54,
-            name: 'black sesame seeds'
+            name: 'black sesame seeds',
+            is_reviewed: true
         },
         {
             id: 55,
-            name: 'salmon, fresh and pin-boned'
+            name: 'salmon, fresh and pin-boned',
+            is_reviewed: true
         },
         {
             id: 56,
-            name: 'sweet chilli sauce'
+            name: 'sweet chilli sauce',
+            is_reviewed: true
         },
         {
             id: 57,
-            name: 'lime'
+            name: 'lime',
+            is_reviewed: true
         },
         {
             id: 58,
-            name: 'fresh ginger'
+            name: 'fresh ginger',
+            is_reviewed: true
         },
         {
             id: 59,
-            name: 'bok choy, kai larn, tat soi or similar'
+            name: 'bok choy, kai larn, tat soi or similar',
+            is_reviewed: true
         },
         {
             id: 63,
-            name: 'breadcrumbs'
+            name: 'breadcrumbs',
+            is_reviewed: true
         },
         {
             id: 64,
-            name: 'garlic powder'
+            name: 'garlic powder',
+            is_reviewed: true
         },
         {
             id: 65,
-            name: 'diced pancetta'
+            name: 'diced pancetta',
+            is_reviewed: true
         },
         {
             id: 66,
-            name: 'fennel'
+            name: 'fennel',
+            is_reviewed: true
         },
         {
             id: 67,
-            name: 'cannellini beans'
+            name: 'cannellini beans',
+            is_reviewed: true
         },
         {
             id: 68,
-            name: 'asparagus tips'
+            name: 'asparagus tips',
+            is_reviewed: true
         },
         {
             id: 69,
-            name: 'sugar snap peas'
+            name: 'sugar snap peas',
+            is_reviewed: true
         },
         {
             id: 70,
-            name: 'frozen peas'
+            name: 'frozen peas',
+            is_reviewed: true
         },
         {
             id: 71,
-            name: 'baby spinach'
+            name: 'baby spinach',
+            is_reviewed: true
         },
         {
             id: 72,
-            name: 'espresso coffee'
+            name: 'espresso coffee',
+            is_reviewed: true
         },
         {
             id: 73,
-            name: 'Marsala wine'
+            name: 'Marsala wine',
+            is_reviewed: true
         },
         {
             id: 74,
-            name: 'ladyfingers'
+            name: 'ladyfingers',
+            is_reviewed: true
         },
         {
             id: 75,
-            name: 'mascarpone'
+            name: 'mascarpone',
+            is_reviewed: true
         },
         {
             id: 76,
-            name: 'rum'
+            name: 'rum',
+            is_reviewed: true
         },
         {
             id: 77,
-            name: ' heavy cream'
+            name: ' heavy cream',
+            is_reviewed: true
         },
         {
             id: 78,
-            name: 'pure vanilla extract'
+            name: 'pure vanilla extract',
+            is_reviewed: true
         },
         {
             id: 79,
-            name: 'apples'
+            name: 'apples',
+            is_reviewed: true
         },
         {
             id: 80,
-            name: 'pastry'
+            name: 'pastry',
+            is_reviewed: true
         },
         {
             id: 81,
-            name: 'brown sugar'
+            name: 'brown sugar',
+            is_reviewed: true
         },
         {
             id: 82,
-            name: 'oranges'
+            name: 'oranges',
+            is_reviewed: true
         },
         {
             id: 83,
-            name: 'strawberries'
+            name: 'strawberries',
+            is_reviewed: true
         },
         {
             id: 84,
-            name: 'raspberries'
+            name: 'raspberries',
+            is_reviewed: true
         },
         {
             id: 85,
-            name: 'blueberries'
+            name: 'blueberries',
+            is_reviewed: true
         },
         {
             id: 86,
-            name: 'bananas'
+            name: 'bananas',
+            is_reviewed: true
         },
         {
             id: 87,
-            name: 'grapefruits'
+            name: 'grapefruits',
+            is_reviewed: true
         },
         {
             id: 88,
-            name: 'avocados'
+            name: 'avocados',
+            is_reviewed: true
         },
         {
             id: 89,
-            name: 'tomatoes'
+            name: 'tomatoes',
+            is_reviewed: true
         },
         {
             id: 90,
-            name: 'pears'
+            name: 'pears',
+            is_reviewed: true
         },
         {
             id: 91,
-            name: 'pumpkin'
+            name: 'pumpkin',
+            is_reviewed: true
         },
         {
             id: 92,
-            name: 'zucchini'
+            name: 'zucchini',
+            is_reviewed: true
         },
         {
             id: 93,
-            name: 'broccoli'
+            name: 'broccoli',
+            is_reviewed: true
         },
         {
             id: 94,
-            name: 'cauliflower'
+            name: 'cauliflower',
+            is_reviewed: true
         },
         {
             id: 95,
-            name: 'silverbeet'
+            name: 'silverbeet',
+            is_reviewed: true
         },
         {
             id: 96,
-            name: 'shallots'
+            name: 'shallots',
+            is_reviewed: true
         },
         {
             id: 97,
-            name: 'cucumber'
+            name: 'cucumber',
+            is_reviewed: true
         },
         {
             id: 98,
-            name: 'lentils'
+            name: 'lentils',
+            is_reviewed: true
         },
         {
             id: 99,
-            name: 'peas'
+            name: 'peas',
+            is_reviewed: true
         },
         {
             id: 100,
-            name: 'tofu'
+            name: 'tofu',
+            is_reviewed: true
         },
         {
             id: 101,
-            name: 'soybeans'
+            name: 'soybeans',
+            is_reviewed: true
         },
         {
             id: 102,
-            name: 'shrimps'
+            name: 'shrimps',
+            is_reviewed: true
         },
         {
             id: 103,
-            name: 'mayonnaise'
+            name: 'mayonnaise',
+            is_reviewed: true
         },
         {
             id: 104,
-            name: 'lemons'
+            name: 'lemons',
+            is_reviewed: true
         },
         {
             id: 105,
-            name: 'capsicum'
+            name: 'capsicum',
+            is_reviewed: true
         },
         {
             id: 106,
-            name: 'eggplant'
+            name: 'eggplant',
+            is_reviewed: true
         },
         {
             id: 107,
-            name: 'lettuce'
+            name: 'lettuce',
+            is_reviewed: true
         },
         {
             id: 108,
-            name: 'fish fillets'
+            name: 'fish fillets',
+            is_reviewed: true
         },
         {
             id: 109,
-            name: 'Balsamic vinegar'
+            name: 'Balsamic vinegar',
+            is_reviewed: true
         },
         {
             id: 110,
-            name: 'bay leaves'
+            name: 'bay leaves',
+            is_reviewed: true
         },
         {
             id: 111,
-            name: 'coriander'
+            name: 'coriander',
+            is_reviewed: true
         },
         {
             id: 112,
-            name: 'sage'
+            name: 'sage',
+            is_reviewed: true
         },
         {
             id: 113,
-            name: 'peppery mint'
+            name: 'peppery mint',
+            is_reviewed: true
         },
         {
             id: 114,
-            name: 'chives'
+            name: 'chives',
+            is_reviewed: true
         },
         {
             id: 115,
-            name: 'lemon grass'
+            name: 'lemon grass',
+            is_reviewed: true
         },
         {
             id: 116,
-            name: 'curry'
+            name: 'curry',
+            is_reviewed: true
         },
         {
             id: 117,
-            name: 'chili powder'
+            name: 'chili powder',
+            is_reviewed: true
         },
         {
             id: 118,
-            name: 'ground nutmeg'
+            name: 'ground nutmeg',
+            is_reviewed: true
         },
         {
             id: 119,
-            name: 'italian herbs'
+            name: 'italian herbs',
+            is_reviewed: true
         },
         {
             id: 120,
-            name: 'sausages'
+            name: 'sausages',
+            is_reviewed: true
         }
     ])
 
