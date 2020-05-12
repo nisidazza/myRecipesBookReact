7ac31428c1e9eed331325e5ddbe09cc69edcc092
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-05-13 10:52:52 +1200

    create AddNewIngredient component - user can add new ingredients in the ingredients table

---
+++ client/components/pages/AddNewIngredient.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+class AddNewIngredient extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {}
+    }
+    
+
+render() {
+    return(
+        <></>
+    )
+}
+
+}
+
+
+
+export default AddNewIngredient
