0824e3101c4ce7a3b3e1176b1bbd8c75006d5f00
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-05-19 12:51:16 +1200

    add demo to homepage

--- client/components/pages/Homepage.jsx
+++ client/components/pages/Homepage.jsx
@@ -26,32 +26,51 @@ class Homepage extends React.Component {
   render() {
     return (
       <div id="Homepage-jsx-component">
-        <div className="row pl-1 row-cols-5">
-          {this.state.recipes.map((recipe, i) => {
-            return (
-              <div className="col p-0" key={i}>
-                <div className="card mx-auto mt-3" style={{width:"14em"}} id="recipe-card">
-                  <img
-                    className="card-img-top"
-                    src={
-                      recipe.img_url ? recipe.img_url : "images/test-image.png"
-                    }
-                    alt="Recipe Image"
-                    height="200em"
-                    style={{ objectFit: "cover" }}
-                  />
-                  <div className="card-body p-2">
-                    <h6 className="card-title mb-2 text-md-left">
-                      <strong>{recipe.title}</strong>
-                    </h6>
-                    <p className="card-subtitle mb-0 text-muted">
-                      Category: {recipe.category}
-                    </p>
+        <div className="row m-0 pl-2">
+          <div className="col-md-8">
+            <div className="embed-responsive embed-responsive-21by9">
+            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/ahCB_8OARCI"></iframe>
+            </div>
+          </div>
+          <div className="col-6 col-md-4"></div>
+        </div>
+        <div className="latest-recipes-container">
+          <div className="row m-0 pl-2">
+            <h5 className="m-0">Latest Recipes</h5>
+          </div>
+          <div className="row m-0 row-cols-5">
+            {this.state.recipes.map((recipe, i) => {
+              return (
+                <div className="col p-0" key={i}>
+                  <div
+                    className="card mx-auto mt-3"
+                    style={{ width: "14em" }}
+                    id="recipe-card"
+                  >
+                    <img
+                      className="card-img-top"
+                      src={
+                        recipe.img_url
+                          ? recipe.img_url
+                          : "images/test-image.png"
+                      }
+                      alt="Recipe Image"
+                      height="200em"
+                      style={{ objectFit: "cover" }}
+                    />
+                    <div className="card-body p-2">
+                      <h6 className="card-title mb-2 text-md-left">
+                        <strong>{recipe.title}</strong>
+                      </h6>
+                      <p className="card-subtitle mb-0 text-muted">
+                        Category: {recipe.category}
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
     );
