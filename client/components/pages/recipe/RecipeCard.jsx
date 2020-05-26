12dd38064466817c0655c3c9df84863319f43036
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-05-26 13:36:54 +1200

    add modal to RecipeCard.jsx to confirm recipe delete

--- client/components/pages/recipe/RecipeCard.jsx
+++ client/components/pages/recipe/RecipeCard.jsx
@@ -2,15 +2,33 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { IfAuthenticated, IfNotAuthenticated } from "../../common/utilities/Authenticated";
 import { getDecodedToken } from "authenticare/client";
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class RecipeCard extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      modal: false
+    }
+  }
+
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal
+    })
+  }
+
+  handleDeleteConfirmation = () => {
+    this.props.onClickDelete()
+    this.setState({
+      modal: false
+    })
   }
 
 
   render() {
-    let {recipe, openInNewTab, onClickDelete} = this.props
+    let {recipe, openInNewTab} = this.props
     const decodedToken = getDecodedToken();
     let userId = -1;
     if (decodedToken) {
@@ -71,10 +89,25 @@ class RecipeCard extends React.Component {
                       <button
                         type="button"
                         className="btn btn-danger"
-                        onClick={onClickDelete}
+                        onClick={this.toggle}
                       >
                         Delete
                       </button>
+                      <Modal isOpen={this.state.modal} toggle={this.toggle}>
+                        <ModalHeader toggle={this.toggle}>ARE YOU SURE?</ModalHeader>
+                        <ModalBody>
+                          Would you like to delete this recipe?
+                        </ModalBody>
+                        <ModalFooter>
+                          <button type="button"
+                        className="btn btn-danger"
+                        onClick={this.handleDeleteConfirmation}
+                        >
+                        Confirm Delete
+                        </button>
+                        <button type="button" className="btn btn-info" onClick={this.toggle}>Cancel</button>
+                        </ModalFooter>
+                      </Modal>
                     </div>
                   </div>
                 ) : (
