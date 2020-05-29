255d98c476ec99c0b3f41cd09c287ccd532eb313
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-05-29 18:20:53 +1200

    Add Google ReCaptcha and Validator to reset token RequestResetToken.jsx

--- client/components/pages/RequestResetToken.jsx
+++ client/components/pages/RequestResetToken.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { apiRequestResetToken } from "../../apis/resetPasswordApi";
+import ReCAPTCHA from "react-google-recaptcha";
+import Validator from "../common/utilities/Validator";
 
 class RequestResetToken extends React.Component {
   possibleOutcomes = {
     undefined: undefined,
     success: "success",
-    error: "error"
+    error: "error",
   };
 
   constructor(props) {
@@ -13,31 +15,67 @@ class RequestResetToken extends React.Component {
 
     this.state = {
       username: "",
-      outcome: this.possibleOutcomes.undefined
+      outcome: this.possibleOutcomes.undefined,
+      isVerified: false,
+      errorMessage: [],
     };
+
+    this.validator = React.createRef();
   }
 
   componentDidMount() {
     let event = new CustomEvent("pageHasChanged", {
-      detail: { pageTitle: "Recovery Email" }
+      detail: { pageTitle: "Recovery Email" },
     });
     document.dispatchEvent(event);
   }
 
-  handleChange = e => {
+  handleChange = (e) => {
     this.state[e.target.id] = e.target.value;
   };
 
-  handleRecoveryEmailSubmit = e => {
+  handleRecoveryEmailSubmit = (e) => {
     e.preventDefault();
-    apiRequestResetToken(this.state.username).then(outcome => {
-      console.log(outcome);
-      this.setState({
-        outcome: outcome.success
-          ? this.possibleOutcomes.success
-          : this.possibleOutcomes.error
+    if (this.validator.current.validate()) {
+      apiRequestResetToken(this.state.username).then((outcome) => {
+        console.log(outcome);
+        this.setState({
+          outcome: outcome.success
+            ? this.possibleOutcomes.success
+            : this.possibleOutcomes.error,
+        });
       });
+    }
+  };
+
+  verifyCaptcha = () => {
+    this.setState({
+      isVerified: true,
     });
+  };
+
+  getValidationRules = () => {
+    let rules = [];
+
+    rules.push({
+      conditional: () => {
+        return (
+          this.state.username &&
+          this.state.username !== "" &&
+          this.state.username.match(/\S+@\S+\.\S+/)
+        );
+      },
+      errorMessage: "Please, insert a valid email address",
+    });
+
+    rules.push({
+      conditional: () => {
+        return this.state.isVerified == true;
+      },
+      errorMessage: "Please verify that you are not a robot",
+    });
+
+    return rules;
   };
 
   render() {
@@ -68,6 +106,12 @@ class RequestResetToken extends React.Component {
                 ></input>
               </div>
             </div>
+            <div className="input-container mx-auto mb-4 col-md-8">
+              <ReCAPTCHA
+                sitekey="6Ld-Dv0UAAAAAK8mlEf9NDEbEtBsO2OHXfKweXFf"
+                onChange={this.verifyCaptcha}
+              ></ReCAPTCHA>
+            </div>
             <div className="input-container mx-auto mt-3 mb-3 col-xs-3">
               {this.state.outcome !== this.possibleOutcomes.success ? (
                 <input
@@ -79,14 +123,15 @@ class RequestResetToken extends React.Component {
                 ""
               )}
             </div>
+            <Validator rules={this.getValidationRules()} ref={this.validator} />
             <div className="request-reset-token-msg">
               {this.state.outcome == this.possibleOutcomes.success ? (
-                <p className='outcome'>
+                <p className="outcome">
                   “An email has been sent to {this.state.username} with further
                   instructions.”
                 </p>
               ) : this.state.outcome == this.possibleOutcomes.error ? (
-                <p className='outcome'>"Sorry, something went wrong!"</p>
+                <p className="outcome">"Sorry, something went wrong!"</p>
               ) : (
                 ""
               )}
