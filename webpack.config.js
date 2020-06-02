3e3c7ca81350b342da87bcf2bc3d4a2bf87e3284
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-06-02 15:05:55 +1200

    install css-loader and style-loader and update webpack-webpack.config.jsconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,36 @@
-const path = require('path')
-const Dotenv = require('dotenv-webpack')
-const InjectEnvironmentPlugin = require('webpack-inject-environment-variables');
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const InjectEnvironmentPlugin = require("webpack-inject-environment-variables");
 
 module.exports = {
-    entry: './client/index.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    mode: 'development',
-    module: {
-        rules: [{
-            test: /\.jsx?$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/,
-            parser: {
-                amd: false
-            }
-        }]
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    plugins: [
-        new Dotenv(),
-        new InjectEnvironmentPlugin({ regex: /^PUBLIC/i }),
+  entry: "./client/index.js",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        parser: {
+          amd: false,
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-    devtool: 'source-map',
-    devServer: {
-        contentBase: './public'
-    }
-}
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [new Dotenv(), new InjectEnvironmentPlugin({ regex: /^PUBLIC/i })],
+  devtool: "source-map",
+  devServer: {
+    contentBase: "./public",
+  },
+};
